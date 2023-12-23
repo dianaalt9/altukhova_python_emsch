8baf9c0f448483787d63fff895d2c72ec6ad73ac
dianaalt9 <134794821+dianaalt9@users.noreply.github.com>
2023-12-24 01:36:52 +0300

    main.py

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("hello, world")
+def convert(s,numRows):
+    snew = ''
+    for i in range(numRows):
+        k = i
+        while k<len(s):
+            snew += s[k]
+            k = k + numRows + 1
+    return snew
+s = input()
+numRows = int(input())
+print(convert(s,numRows))
